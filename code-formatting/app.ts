76d3dad97c8d06780b9d9cb6b87ba075fcae89e6
softmarshmallow <woojoo@softmarshmallow.com>
2021-07-13 22:45:08 +0900

    add `format` api route

--- code-formatting/app.ts
+++ code-formatting/app.ts
@@ -1,13 +1,59 @@
+import { format as format_yaml } from "./yaml-format";
+import { format as format_typescript } from "./typescript-format";
+import { format as format_javascript } from "./javascript-format";
+import { format as format_dart } from "./dart-format";
+import { format as format_html } from "./html-format";
+
 import express from "express";
 const app = express();
 
-app.all("/", (req, res, next) => {
+app.post("/format", (req, res, next) => {
+  //
+  const { code } = req.body;
+  const { lang } = req.query;
+  //
+  console.log("lang:code", lang, code);
+  let formatted: string;
+  switch (lang) {
+    case "DART":
+    case "Dart":
+    case "dart":
+      formatted = format_dart(code);
+      break;
+    case "js":
+    case "JS":
+    case "JavaScript":
+    case "javascript":
+    case "JAVASCRIPT":
+    case "flow":
+    case "Flow":
+    case "FLOW":
+      formatted = format_javascript(code);
+      break;
+
+    case "ts":
+    case "TS":
+    case "tsx":
+    case "TSX":
+    case "TypeScript":
+    case "typescript":
+    case "TYPESCRIPT":
+      formatted = format_typescript(code);
+      break;
+
+    default:
+      return res.status(400).json({
+        error: "can not recognize language",
+        code: code,
+      });
+  }
   return res.status(200).json({
-    message: "Hello from root!",
+    lang: lang,
+    code: formatted,
   });
 });
 
-app.get("/hello", (req, res, next) => {
+app.get("/", (req, res, next) => {
   return res.status(200).json({
     message: "Hello from path!",
   });
