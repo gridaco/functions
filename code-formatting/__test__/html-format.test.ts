5ec0fbf15202d1ab5499c98daa9fd233ba3cfc5c
softmarshmallow <woojoo@softmarshmallow.com>
2021-07-22 14:30:42 +0900

    update test cases - all passing

--- code-formatting/__test__/html-format.test.ts
+++ code-formatting/__test__/html-format.test.ts
@@ -1,7 +1,7 @@
 import { format } from "../html-format";
-const HTML_PLAIN = `void main( ){}
+const HTML_PLAIN = `<div></div>
 `;
-const HTML_STANDARD = `void main() {}
+const HTML_STANDARD = `<div></div>
 `;
 test("format html code", () => {
   expect(format(HTML_PLAIN)).toBe(HTML_STANDARD);
--- code-formatting/__test__/ts-format.test.ts
+++ code-formatting/__test__/ts-format.test.ts
@@ -1,16 +1,20 @@
 import { format } from "../typescript-format";
 const TS_PLAIN = `test("format ts code", () => {expect(format(TS_PLAIN)).toBe(TS_STANDARD)})
 `;
-const TS_STANDARD = `void main() {}
+const TS_STANDARD = `test("format ts code", () => {
+  expect(format(TS_PLAIN)).toBe(TS_STANDARD);
+});
 `;
 
 test("format ts code", () => {
   expect(format(TS_PLAIN)).toBe(TS_STANDARD);
 });
 
-const TSX_PLAIN = `void main( ){}
+const TSX_PLAIN = `function Button() { return ( <button>click me</button>);}
 `;
-const TSX_STANDARD = `void main() {}
+const TSX_STANDARD = `function Button() {
+  return <button>click me</button>;
+}
 `;
 
 test("format tsx code", () => {
